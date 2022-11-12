d8dea23392a294eb594dbe81c6152708a03a599c
Konstantin Obenland <obenland@gmx.de>
2022-11-11 22:20:17 -0700

    Simplify uninstall routine (#10)

--- uninstall.php
+++ uninstall.php
@@ -2,7 +2,7 @@
 /**
  * Uninstall.
  *
- * @package WP Last Login
+ * @package wp-last-login
  */
 
 // Don't uninstall unless you absolutely want to!
@@ -10,15 +10,7 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	wp_die( 'WP_UNINSTALL_PLUGIN undefined.' );
 }
 
-$user_ids = get_users( array(
-	'blog_id' => '',
-	'fields'  => 'ID',
-) );
-
-foreach ( $user_ids as $user_id ) {
-	// phpcs:ignore WordPress.VIP.RestrictedFunctions.user_meta_delete_user_meta
-	delete_user_meta( $user_id, 'wp-last-login' );
-}
+delete_metadata( 'user', 0, 'wp-last-login', '', true );
 
 
 /* Goodbye! Thank you for having me! */
